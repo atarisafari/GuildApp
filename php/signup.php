ff1d54a1418831cdfc1a86acf45f9c110db21657
Ethan Finlay <efinclarke1993@gmail.com>
2019-03-08 21:12:21 -0500

    Error Checking
    - Now returns JSON payload
    - Now returns error message if username is taken

--- php/signup.php
+++ php/signup.php
@@ -3,15 +3,33 @@
 include("establishConn.php");
 
 //Prepare and bind for insertion
-$sql = $conn->prepare("INSERT INTO user_info (username, password) VALUES(?, ?)"$
+$sql = $conn->prepare("INSERT INTO user_info (username,$
+$test = $conn->prepare("SELECT user_id  FROM user_info $
+
+$test->bind_param("s",$username);
 $sql->bind_param("ss", $username, $password);
 
 //Get data from the JSON post
-$input = json_decode(file_get_contents('php://input'), true);
-
-//Need to add a check to see if username is taken
+$input = json_decode(file_get_contents('php://input'), $
 
 $username = $input["username"];
 $password = $input["password"];
 
-$sql->execute();
+$test->execute();
+$result = $test->get_result();
+
+if($result->num_rows > 0)
+{
+        $payload = '{"error": "Username taken"}';
+
+}
+
+else
+{
+        $sql->execute();
+        $payload = '{"error": ""}';
+}
+
+header('Content-type: application/json');
+echo $payload;
+                    
