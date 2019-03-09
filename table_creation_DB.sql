d3f7bdcbf85f0b25dfe0bfd93174aae751156bae
Ethan Finlay <efinclarke1993@gmail.com>
2019-03-08 20:46:07 -0500

    Comment and Likes tables added

--- table_creation_DB.sql
+++ table_creation_DB.sql
@@ -9,23 +9,44 @@ CREATE TABLE user_info
 
 CREATE TABLE user_relationship
 (
-    user_first_id INT,
-    user_second_id INT,
+    user_first_id INT NOT NULL,
+    user_second_id INT NOT NULL,
     type ENUM('pending_first_second', 'pending_second_first','friends','block_first_second', 'block_second_first','block_both') default NULL,
     PRIMARY KEY(user_first_id, user_second_id)
 );
 
---Create table for Posts
-CREATE TABLE post
+
+CREATE TABLE posts
 (
     post_id INT AUTO_INCREMENT,
-    user_id INT,
+    user_id INT NOT NULL,
     content VARCHAR(2000) DEFAULT NULL,
     image_url VARCHAR(255) DEFAULT NULL,
-    date_created DATE,
-    time_created TIMESTAMP,
+    date_created DATE NOT NULL,
+    time_created TIMESTAMP NOT NULL,
     PRIMARY KEY(post_id),
     FOREIGN KEY(user_id) REFERENCES user_info(user_id) ON DELETE CASCADE
 );
 
---Create table for Comments
+CREATE TABLE comments
+(
+    comment_id INT AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    content VARCHAR(1000),
+    date_created DATE NOT NULL,
+    time_created TIMESTAMP NOT NULL,
+    PRIMARY KEY(comment_id),
+    FOREIGN KEY(user_id) REFERENCES user_info(user_id) ON DELETE CASCADE,
+    FOREIGN KEY(post_id) REFERENCES posts(post_id) ON DELETE CASCADE 
+ );
+ 
+ CREATE TABLE likes
+ (
+    like_id INT AUTO_INCREMENT
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY(like_id),
+    FOREIGN KEY(post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
+    FOREIGN KEY(user_id) REFERENCES user_info(user_id) ON DELETE CASCADE   
+  );
