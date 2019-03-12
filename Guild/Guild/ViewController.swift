bd77cebfe34281c1cd7158b2e57ee28a552909e1
juccalle <46544460+juccalle@users.noreply.github.com>
2019-03-11 22:10:25 -0400

    added login capability

--- Guild/Guild/ViewController.swift
+++ Guild/Guild/ViewController.swift
@@ -6,9 +6,44 @@
 //  Copyright © 2019 JCalle. All rights reserved.
 //
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    let URL = "http://157.230.66.35/php/login.php"
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBAction func SignIn(_ sender: UIButton) {
+        
+        let loginRequest: Parameters = [
+            "username" : email.text!,
+            "password" : password.text!
+        ]
+
+        AF.request(URL, method: .post, parameters: loginRequest,encoding: JSONEncoding.default).responseJSON
+            {
+                response in
+                //printing response
+                print(response)
+                
+                if let result = response.result.value {
+                    
+                    //converting it as NSDictionary
+                    let jsonData = result as! NSDictionary
+                    
+                    let test = jsonData.value(forKey: "error") as! String?
+                    
+                    if test != "login failed"{
+                        print("worked!")
+                    }
+                }
+                
+            }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
