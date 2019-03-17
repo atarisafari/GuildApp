d2bdd9c3201c531d1514189ce83008d0a5645225
Fernando Trevino <trevino.96@hotmail.com>
2019-03-17 17:41:26 -0400

    typos and change some NULL -> '  '
    change some NULL -> '  '  to escape some common errors

--- Website/table_creation_DB.sql
+++ Website/table_creation_DB.sql
@@ -3,7 +3,7 @@ CREATE TABLE user_info
     user_id INT AUTO_INCREMENT,
     username VARCHAR(20) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    display_name VARCHAR(20) DEFAULT NULL,
+    display_name VARCHAR(20) DEFAULT ' ',
     profile_pic_url VARCHAR(50) DEFAULT NULL,
     PRIMARY KEY (user_id)
 );
@@ -13,7 +13,7 @@ CREATE TABLE user_relationship
 (
     user_first_id INT NOT NULL,
     user_second_id INT NOT NULL,
-    type ENUM('pending_first_second', 'pending_second_first','friends','block_first_second', 'block_second_first','block_both') default NULL,
+    type ENUM('pending_first_second', 'pending_second_first','friends','block_first_second', 'block_second_first','block_both') DEFAULT ' ',
     PRIMARY KEY(user_first_id, user_second_id)
 );
 
@@ -22,7 +22,7 @@ CREATE TABLE posts
 (
     post_id INT AUTO_INCREMENT,
     user_id INT NOT NULL,
-    content VARCHAR(2000) DEFAULT NULL,
+    content VARCHAR(2000) DEFAULT ' ',
     image_url VARCHAR(255) DEFAULT NULL,
     time_created TIMESTAMP NOT NULL,
     num_likes INT NOT NULL DEFAULT '0',
@@ -36,7 +36,7 @@ CREATE TABLE comments
     comment_id INT AUTO_INCREMENT,
     user_id INT NOT NULL,
     post_id INT NOT NULL,
-    content VARCHAR(1000),
+    content VARCHAR(1000) DEFAULT ' ',
     time_created TIMESTAMP NOT NULL,
     PRIMARY KEY(comment_id),
     FOREIGN KEY(user_id) REFERENCES user_info(user_id) ON DELETE CASCADE,
