0984d172b34fe6059d6c71c6f8192c61850c500d
Fernando Trevino <trevino.96@hotmail.com>
2019-03-17 18:12:56 -0400

    Helper functions for reference
    And to ease back end queries

---
+++ Website/helper_functions.sql
@@ -0,0 +1,43 @@
+--Signup
+INSERT INTO user_info (username, password) 
+VALUES(?, ?);
+
+--Update Display Name and profile pic
+UPDATE user_info
+SET display_name  = ?, profile_pic_url = ?
+WHERE user_info.user_id  = ?;
+
+--Login
+SELECT * 
+FROM user_info 
+WHERE username=? and password=?;
+
+--Add Post
+INSERT INTO posts (user_id,content,image_url,date_created,time_created) 
+VALUES( ?, ?, ?, ?, ?, LEFT( ?, 32) );
+
+--Delete Post
+DELETE FROM posts
+WHERE posts.post_id = ?;
+
+--Like Post
+
+
+--Add Comment to Post
+
+
+--Changing a relationship with a friend will be done the same but with different input for Add, Delete and Block
+  --Add Friend
+INSERT INTO ()
+
+  --Delete Friend
+
+
+  --Block Friend
+
+
+--We might add groups or blocks for specific posts
+
+
+--WARNING NEXT COMAND IS TO ERRASE ALL CONTENT IN THE DATABASE
+DROP TABLE *;
