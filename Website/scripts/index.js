11c39a3dcf34fb38d408c1abdef63988ad3e8fd5
Fernando Trevino <trevino.96@hotmail.com>
2019-03-26 17:16:48 -0400

    Update index.js

--- Website/scripts/index.js
+++ Website/scripts/index.js
@@ -3,34 +3,55 @@ import React, { Component } from 'react';
 let baseURL = "/php";
 
 //TODO SignUp
+//NEED: Username, Password, Display Name and Profile picture url
 
-//TODO LogOut
+//TODO LogOut 
+//NEED: errase Local Storage and Cookies
 
 //TODO Update Display Name and profile pic
+//NEED:User_id, Display Name and profile pic
 
 //TODO Login
+//NEED: Username and Password
+
+//TODO Check for Friend request
+//NEED: user_id
 
 //TODO AddPost
+//NEED: user_id,content,image_url,date_created,time_created
 
 //TODO DeletePost
+//NEED: post_id and user_id
 
 //TODO FetchXPosts from a friend
+//NEED: username (friend)
 
 //TODO AddComment
+//NEED: comment_id , user_id , post_id, content, time_created
+
+//TODO DeleteComment
+//NEED: user_id, post_id, comment_id 
 
 //TODO EditComment
+//NEED: user_id, post_id, comment_id 
 
 //TODO FetchXComments from a post
+//NEED: post_id
 
 //TODO AddFriend
+//NEED: user_id (own) , username (friend)
 
 //TODO DeleteFriend
+//NEED: both user_id 
 
 //TODO BlockFriend
+//NEED: both user_id
 
 //TODO FetchAllFriends
+//NEED: both user_id
 
 //TODO Like/Unlike Post
+//NEED:
 /*--Check if post has been liked by user
   --If no time is finded, then post not liked
 */
