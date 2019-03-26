5c236dfeff65abc03b4caf236d85604d863d92b6
Fernando Trevino <trevino.96@hotmail.com>
2019-03-26 16:44:44 -0400

    Update index.js

--- Website/scripts/index.js
+++ Website/scripts/index.js
@@ -1,1 +1,36 @@
+import React, { Component } from 'react';
+
 let baseURL = "/php";
+
+//TODO SignUp
+
+//TODO LogOut
+
+//TODO Update Display Name and profile pic
+
+//TODO Login
+
+//TODO AddPost
+
+//TODO DeletePost
+
+//TODO FetchXPosts from a friend
+
+//TODO AddComment
+
+//TODO EditComment
+
+//TODO FetchXComments from a post
+
+//TODO AddFriend
+
+//TODO DeleteFriend
+
+//TODO BlockFriend
+
+//TODO FetchAllFriends
+
+//TODO Like/Unlike Post
+/*--Check if post has been liked by user
+  --If no time is finded, then post not liked
+*/
