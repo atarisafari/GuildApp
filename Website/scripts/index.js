b608bb699d884b03275ad31941e299ab2e722261
Fernando Trevino <trevino.96@hotmail.com>
2019-04-08 12:09:00 -0400

    Update index.js

--- Website/scripts/index.js
+++ Website/scripts/index.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Router from './pages/router'
+import * as serviceWorker from './serviceWorker';
 
-let baseURL = "/php";
+ReactDOM.render(<Router />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 //TODO SignUp
 //NEED: Username, Password, Display Name and Profile picture url
