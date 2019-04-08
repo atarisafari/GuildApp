1635d1e91cecd7d468ad424f518d2044413db381
Fernando Trevino <trevino.96@hotmail.com>
2019-04-08 11:52:21 -0400

    Create apiCalls.js

---
+++ Website/scripts/utils/apiCalls.js
@@ -0,0 +1,22 @@
+//const fetch = require('node-fetch');
+export const signUp = async (username,password,display_name='',profile_pic_url='') => { 
+        fetch('http://157.230.66.35/php/signup.php', {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            display_name: display_name,
+            profile_pic_url: profile_pic_url,
+        })
+        }).then(response => response.json())
+        .then(res => {
+        console.log('response:');
+        console.log(res);
+        })
+        .catch( e => console.log(e));
+    }
