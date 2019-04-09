0102b4a211aa5e5c9696f6bb9851e70405929980
Fernando Trevino <trevino.96@hotmail.com>
2019-04-09 14:22:04 -0400

    Update signUp.js

--- Website/scripts/pages/signUp.js
+++ Website/scripts/pages/signUp.js
@@ -1,6 +1,7 @@
 
 import React, { useContext, useState } from 'react'
 import {signUp} from '../utils/testAPI';
+import HomeButton from '../components/buttons/homeButton';
 
 export default props => {
     // const {message} = useContext(FBContext); 
@@ -23,32 +24,37 @@ export default props => {
     const profile_pic_urlHandler = profile_pic_url=>{
         setprofile_pic_url(profile_pic_url);
     }
+    const signUpHandler = async() =>{
+        let data = await signUp(username,password,display_name,profile_pic_url);
+        console.log("Result" , data);
+        if(data !== null){
+            //TODO
+        }
+    }
 //    const charSelectHandler = event=>{
 //        const charId = event.target.value;
 //        setSelectCharacter(charId);
 //    }
-
+    //console.log(username,password);
     return (
     <div className="App">
         <h1> SignUp </h1> 
         {/* <SamplePage2 />  */}
         <div id="signUp">
-            <form>
-                <div id="username_signUp">
-                    Username:
-                    <input value={username} onChange= { e => usernameHandler(e.target.value)}/>
-                </div>
-                <div id="password_signUp">
-                    Password: 
-                    <input type='password' value={password} onChange= { e => passwordHandler(e.target.value)}/>
-                </div>
-                <div id="password_signUp">
-                    Display Name: 
-                    <input value={display_name} onChange= { e => display_nameHandler(e.target.value)}/>
-                </div>
-                <button onClick={()=>signUp(username,password,display_name,profile_pic_url)}> SIGN UP </button>
-                <button onClick={()=>props.history.push('/')}> BACK </button>
-            </form>
+            <div id="username_signUp">
+                Username:
+                <input onBlur= { e => usernameHandler(e.target.value)}/>
+            </div>
+            <div id="password_signUp">
+                Password: 
+                <input type='password' onBlur= { e => passwordHandler(e.target.value)}/>
+            </div>
+            <div id="password_signUp">
+                Display Name: 
+                <input onBlur= { e => display_nameHandler(e.target.value)}/>
+            </div>
+            <button onClick={()=>signUpHandler()}> SIGN UP </button>
+            <HomeButton path='/' {...props}>BACK</HomeButton>
         </div>
     </div>
     )
