95a2ffb091e789b82bc02e2f3f2e90d7498e6944
trevinofernando <trevino.96@hotmail.com>
2019-04-12 12:52:33 -0400

    Fixed .gitignore, ignoring build

---
+++ web/build/precache-manifest.9c45f29a81a90f19856fa81ce70760b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac71d15155bd3628f3a5",
+    "url": "/static/js/main.25d13b08.chunk.js"
+  },
+  {
+    "revision": "88b7cc68cb3295f6bc09",
+    "url": "/static/js/2.0be55ff4.chunk.js"
+  },
+  {
+    "revision": "ac71d15155bd3628f3a5",
+    "url": "/static/css/main.52be734d.chunk.css"
+  },
+  {
+    "revision": "3876a2b0850e14d6dc4a4f02b10d666f",
+    "url": "/index.html"
+  }
+];
