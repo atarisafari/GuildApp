378c25fc1cbbd04014b01c8cd7a40e6382202685
LIAMJARVIS <lfjarvis@gmail.com>
2019-04-15 22:04:32 -0400

    athentication navigation working

--- ReactMobile/navigation/AppNavigator.js
+++ ReactMobile/navigation/AppNavigator.js
@@ -3,9 +3,16 @@ import { createAppContainer, createSwitchNavigator, createStackNavigator } from 
 
 import MainTabNavigator from './MainTabNavigator';
 import AuthenticationNavigator from './AuthenticationNavigator';
+import Authenticate from '../screens/AuthenticationScreen';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
+
+	Main: MainTabNavigator,
+	Auth: Authenticate
+},{
+	initialRouteName: "Auth"
+}
+	
+));
