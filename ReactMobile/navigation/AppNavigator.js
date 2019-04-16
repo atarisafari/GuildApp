6a15db94a0bf1c2ff8f91e026cc7b9c8f2a97fb2
LIAMJARVIS <lfjarvis@gmail.com>
2019-04-15 23:49:06 -0400

    Login Screen good, need API calls

--- ReactMobile/navigation/AppNavigator.js
+++ ReactMobile/navigation/AppNavigator.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
-import AuthenticationNavigator from './AuthenticationNavigator';
-import Authenticate from '../screens/AuthenticationScreen';
+import LoginScreen from '../screens/AuthenticationScreen';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
 
 	Main: MainTabNavigator,
-	Auth: Authenticate
+	Auth: LoginScreen
 },{
 	initialRouteName: "Auth"
 }
