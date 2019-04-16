f016ffde118a7f3f589c4c83123defa9d0955458
Jiayi Zhang <35317400+jiayizhg@users.noreply.github.com>
2019-04-16 14:15:34 -0400

    Add post

--- web/src/components/Post.js
+++ web/src/components/Post.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button } from 'reactstrap';
+import LikeButton from '@material-ui/icons/FavoriteBorder';
+import CommentButton from '@material-ui/icons/Comment';
+import ToggleIcon from 'material-ui-toggle-icon'
+import Badge from '@material-ui/core/Badge';
+import IconButton from '@material-ui/core/IconButton'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
 
 const Post = (props) => {
     return (
         <div>
+            
         <Card style={{ width: '30rem' }}>
             <CardBody>
-            <input></input>
-            <Button variant="primary">Post</Button>
+            <CardTitle tag="h1"> {props.username}</CardTitle>
+            <CardText tag="p">{props.content}</CardText>
+            
+            {/*Like Button*/}
+            <IconButton>
+                
+                <LikeButton />
+                
+            </IconButton>
+
+            {/*Comment Button*/}
+
+            <IconButton >
+            
+                <CommentButton />
+                
+            </IconButton>
+
             </CardBody>
         </Card>
         </div>
     );
 };
 
+
 export default Post;
+
--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,13 +1,26 @@
 
 import React, { useContext, useState, useEffect  } from 'react'
+import {addPost} from '../utils/apiCalls';
 import Friend from '../components/Friend';
 import Post from '../components/Post';
 import Header_component from '../components/header/Header_component';
+import axios from 'axios';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+import Popup from "reactjs-popup";
+import Camera from '@material-ui/icons/CameraAlt';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+import Divider from '@material-ui/core/Divider';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 export default props => {
+    console.log('props: ', props); 
     const token = localStorage.getItem('token');
-    const [username,setusername] = useState('');
     //const friends = grabAllFriends(token);
+    const [content,setContent] = useState('');
     const friends = [{
         name: 'Jon',
         username: 'DogMan',
@@ -62,9 +75,27 @@ export default props => {
         props.history.push("/");
     }
 
+    const contentHandler = content=>{
+        setContent(content);
+        console.log(content);
+    }
+
+    const addPostHandler = async() =>{
+        
+        let data = await addPost(token, content);
+        console.log("Result" , data);
+        if(data.error === ""){
+            console.log("Add post was successful");
+        }
+        else{
+            alert(data.error);
+        }
+    }
+
     return (
         <div className="App">
             <Header_component props={props}/>
+            
             <h1> Home Page </h1> 
             {/* <Home />  */}
             {
@@ -72,12 +103,55 @@ export default props => {
                     return <Friend key={index} name={value.name} username={value.username}  /*{...props}*/></Friend>
                 })
             }
+            
             {
                 posts.map((value, index) => {
-                    return <Post key={index} username={value.username}  /*{...props}*/></Post>
+                    return <div >
+                        <Post key={index} username={value.username}  content={value.content}/*{...props}*/></Post>
+                        </div>
                 })
             }
-            
+            <div id="content_post">
+            <Card style={{ width: '25rem' }}>
+            <Popup trigger={
+                <button>
+                    <TextField 
+                        id="textPopUp" 
+                        fullWidth 
+                        placeholder="Add a post..." 
+                        InputProps={{
+                            endAdornment: <InputAdornment position="end"> <Camera /></InputAdornment>,
+                        }}
+                    /> 
+                < /button>
+                } position="bottom center" modal > 
+
+            {cancel => (
+                <div id="cancel">
+                    <CardTitle tag="h1"> User Display Name</CardTitle>
+                    <CardBody>
+                        <div id="postContent">
+                            <form autoComplete="off">
+                            <TextField id="textArea" placeholder="Add a post..." fullWidth margin="none" multiline rows="2" onBlur= {e => contentHandler(e.target.value)}/>
+                            </form>
+                        </div>
+                        
+                        <div id="postImage">
+                            <IconButton><Camera /> </IconButton>
+                        </div>
+                       
+                        <div id="buttons">
+                        <Button type="submit" class="btn btn-primary" onClick={()=>addPostHandler()}>POST</Button>
+                        <a href="#" className="cancel" onClick={cancel}> Cancel </a>
+                        </div>
+                   
+                    </CardBody>
+                </div>
+                )}
+            </Popup>
+                
+                </Card>
+            </div>
             <button onClick={()=>logout()}> LOG OUT </button>
         </div>
     )
