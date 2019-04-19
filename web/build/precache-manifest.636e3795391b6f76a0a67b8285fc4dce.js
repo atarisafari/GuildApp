488aca8a930061e74bc6f73b512f3c2cb9385a6f
trevinofernando <trevino.96@hotmail.com>
2019-04-19 18:54:24 -0400

    added Juan css to build

---
+++ web/build/precache-manifest.636e3795391b6f76a0a67b8285fc4dce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e41b57646457d81bebb26fe1922dbc41",
+    "url": "/static/media/2_BowArrow_Icon.e41b5764.png"
+  },
+  {
+    "revision": "7c6be7d8a9f892d17feb",
+    "url": "/static/css/main.4c0ac2ce.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9ac72bcaaedcc8dbd175ec54eee8283",
+    "url": "/static/media/Guild_Logo.c9ac72bc.png"
+  },
+  {
+    "revision": "1e767e21aa5ba214941f",
+    "url": "/static/js/2.b1bbfd17.chunk.js"
+  },
+  {
+    "revision": "ad1f5489aa9e17571df4083c4d5aa13f",
+    "url": "/static/media/0_GuildSword_Icon.ad1f5489.png"
+  },
+  {
+    "revision": "b272469276867143a7d46924c2489232",
+    "url": "/static/media/1_Sword_Icon.b2724692.png"
+  },
+  {
+    "revision": "7c6be7d8a9f892d17feb",
+    "url": "/static/js/main.75d177a1.chunk.js"
+  },
+  {
+    "revision": "b3bde274b037220926864855950d4819",
+    "url": "/static/media/3_Staff_Icon.b3bde274.png"
+  },
+  {
+    "revision": "d12d4b5a22da48febad1570fe34cedb3",
+    "url": "/static/media/4_Shield_Icon.d12d4b5a.png"
+  },
+  {
+    "revision": "eb16ecf44cfdb16fefa7bcb7acf968c1",
+    "url": "/static/media/logo.eb16ecf4.png"
+  },
+  {
+    "revision": "1d88924ddec025ab55db4ed83307dad2",
+    "url": "/static/media/profilePicPlaceholder.1d88924d.png"
+  },
+  {
+    "revision": "1e767e21aa5ba214941f",
+    "url": "/static/css/2.462773fc.chunk.css"
+  },
+  {
+    "revision": "c082cd8d24a412ebc8fbf19e0d5b98b0",
+    "url": "/index.html"
+  }
+];
--- web/build/service-worker.js
+++ web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab26fc2a686ce78a745cd21edc99b243.js"
+  "/precache-manifest.636e3795391b6f76a0a67b8285fc4dce.js"
 );
 
 workbox.clientsClaim();
